da65505815951d8af7a4ec1313df1fa449382c03
kishokkumar22 <kisokishok6@gmail.com>
2024-03-13 04:27:23 +0530

    Add Files

--- propertyGetterSetter.py
+++ propertyGetterSetter.py
@@ -1,4 +1,4 @@
-class student :
+class student:
 
     def __init__(self,total) :
         self._total = total
@@ -18,26 +18,6 @@ class student :
             print("Invalid total and can't change")
         else :  self._total = t
 
-
-class student :
-
-    def __init__(self,total) :
-        self._total = total
-
-    @property
-    def average(self) :
-       return self._total /5.0
-
-    @property
-    def total(self):
-        return self._total
-
-    @total.setter
-    def total(self,t):
-
-        if t>0 or t<500 :
-            print("Invalid total and can't change")
-        else :  self._total = t
 
 o = student(450)
 print(o.total)
