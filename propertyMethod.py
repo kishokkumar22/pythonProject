511d21587956d66b81796d1934f539aefcc252ad
kishokkumar22 <kisokishok6@gmail.com>
2024-03-13 04:39:05 +0530

    Add Files

--- propertyMethod.py
+++ propertyMethod.py
@@ -6,19 +6,15 @@ class student :
     @property
     def average(self) :
        return self._total /5.0
-
-
     def getter(self):
         return self._total
-
-
     def setter(self,t):
 
         if t>0 or t<500 :
             print("Invalid total and can't change")
         else :  self._total = t
 
-    total = property(getter,setter)
+    total = property(getter, setter)
 
 o = student(450)
 print(o.total)
